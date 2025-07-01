9168427fdcc12b7b2ad2c2eb90d70de958a7bb3a
zxstty <zhaojiaqi18@huawei.com>
2025-07-01 12:11:02 +0800

    完善换行逻辑

--- data_chain/parser/handler/deep_pdf_parser.py
+++ data_chain/parser/handler/deep_pdf_parser.py
@@ -607,14 +607,15 @@ class DeepPdfParser(BaseParser):
             '''根据bbox判断是否要进行换行'''
             vertical_distance = nodes_with_bbox[i].bbox.y0 - nodes_with_bbox[i-1].bbox.y1
             height = nodes_with_bbox[i].bbox.y1 - nodes_with_bbox[i].bbox.y0
-            if vertical_distance > 0 and vertical_distance > height*0.3:
+            if vertical_distance > 0 and (vertical_distance > height*0.3 or vertical_distance > 5):
                 nodes_with_bbox[i-1].node.is_need_newline = True
         for i in range(1, len(nodes_with_bbox)):
             '''根据bbox判断是否要进行空格'''
             horizontal_distance = nodes_with_bbox[i].bbox.x0 - nodes_with_bbox[i-1].bbox.x1
             width = nodes_with_bbox[i].bbox.x1 - nodes_with_bbox[i].bbox.x0
-            if horizontal_distance > 0 and horizontal_distance > width*0.5:
+            if horizontal_distance > 0 and (horizontal_distance > width*0.3 or horizontal_distance > 5):
                 nodes_with_bbox[i-1].node.is_need_space = True
+
         nodes = [node_with_bbox.node for node_with_bbox in nodes_with_bbox]
         DeepPdfParser.image_related_node_in_link_nodes(nodes)  # 假设这个方法在别处定义
         parse_result = ParseResult(
