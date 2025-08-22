af937d584609718e863bccee87a905b23c40a985
zxstty <zhaojiaqi18@huawei.com>
2025-08-22 12:00:19 +0800

    修复检索chunk返回作者名称的需要不为空的bug

--- data_chain/entities/response_data.py
+++ data_chain/entities/response_data.py
@@ -325,8 +325,8 @@ class UpdateChunkEnabledResponse(ResponseData):
 class DocChunk(BaseModel):
     """Post /chunk/search 数据结构"""
     doc_id: uuid.UUID = Field(description="文档ID", alias="docId")
-    doc_name: str = Field(description="文档名称", alias="docName")
-    doc_author: str = Field(description="文档作者", alias="docAuthor")
+    doc_name: str = Field(default="", description="文档名称", alias="docName")
+    doc_author: str = Field(default="", description="文档作者", alias="docAuthor")
     doc_abstract: str = Field(default="", description="文档摘要", alias="docAbstract")
     doc_extension: str = Field(default="", description="文档扩展名", alias="docExtension")
     doc_size: int = Field(default=0, description="文档大小，单位是KB", alias="docSize")
