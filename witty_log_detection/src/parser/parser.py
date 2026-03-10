4ed4e62ba41e80a0b961206ddcf277139b26de3d
root <zhaojiaqi18@huawei.com>
2026-03-10 16:08:49 +0800

    修复基于大模型的异常日志解析最后一段没有存入列表的bug

--- witty_log_detection/src/parser/parser.py
+++ witty_log_detection/src/parser/parser.py
@@ -12,6 +12,7 @@ class LogParser:
     """
     日志解析器，负责从日志文件中提取日志信息，并进行日志类型分类
     """
+
     @staticmethod
     def get_log_line_type(log_line: str) -> LogTypeEnum:
         """
@@ -21,12 +22,17 @@ class LogParser:
         for log_type in LogTypeEnum:
             if log_type in log_feature_class_mapping:
                 keywrods_regex_and_scores = log_feature_class_mapping[
-                    log_type].keywords_regex_and_scores
+                    log_type
+                ].keywords_regex_and_scores
                 score = 0
-                for keyword_regex, keyword_score in keywrods_regex_and_scores["normal"].items():
+                for keyword_regex, keyword_score in keywrods_regex_and_scores[
+                    "normal"
+                ].items():
                     if re.search(keyword_regex, log_line):
                         score += keyword_score
-                for keyword_regex, keyword_score in keywrods_regex_and_scores["anomalous"].items():
+                for keyword_regex, keyword_score in keywrods_regex_and_scores[
+                    "anomalous"
+                ].items():
                     if re.search(keyword_regex, log_line):
                         score += keyword_score
                 score_dict[log_type] = score
@@ -48,7 +54,7 @@ class LogParser:
         if re.match(image_end, file_path):
             log_lines = asyncio.run(OcrTool.image_to_text_list(file_path))
         if re.match(text_end, file_path):
-            with open(file_path, 'r', encoding='utf-8', errors='ignore') as f:
+            with open(file_path, "r", encoding="utf-8", errors="ignore") as f:
                 for line in f:
                     log_lines.append(line.strip())
         return log_lines
@@ -59,26 +65,35 @@ class LogParser:
         # 这里实现日志内容脱敏的具体逻辑
         log_content = log_model.content
         log_type: LogTypeEnum = log_model.log_type
-        value_and_value_mask = [(LogValueEnum.IP, "<ip>"), (LogValueEnum.TIMESTAMP, "<timestamp>"),
-                                (LogValueEnum.LEVEL, "<level>")]
+        value_and_value_mask = [
+            (LogValueEnum.IP, "<ip>"),
+            (LogValueEnum.TIMESTAMP, "<timestamp>"),
+            (LogValueEnum.LEVEL, "<level>"),
+        ]
         masked_content = log_content[:]
         for log_value_enum, mask in value_and_value_mask:
             regex = log_feature_class_mapping[log_type].capture_patterns.get(
-                log_value_enum.value, None)
+                log_value_enum.value, None
+            )
             if regex is not None:
-                masked_content = re.sub(
-                    regex, mask, masked_content)
+                masked_content = re.sub(regex, mask, masked_content)
         return masked_content
 
     @staticmethod
-    async def get_log_templates(log_models: list[LogModel], batch_size: int = 8192, need_embedding: bool = False) -> None:
+    async def get_log_templates(
+        log_models: list[LogModel], batch_size: int = 8192, need_embedding: bool = False
+    ) -> None:
         """获取日志模板"""
         # 这里实现获取日志模板的具体逻辑
         log_templates = []
         for i in range(0, len(log_models), batch_size):
-            batch_log_models = log_models[i:i+batch_size]
+            batch_log_models = log_models[i : i + batch_size]
             masked_contents = await asyncio.gather(
-                *[LogParser.mask_log_content(log_model) for log_model in batch_log_models])
+                *[
+                    LogParser.mask_log_content(log_model)
+                    for log_model in batch_log_models
+                ]
+            )
             log_templates += masked_contents
         if need_embedding:
             log_template_embeddings = await Embedding.vectorize_embedding(log_templates)
@@ -88,7 +103,11 @@ class LogParser:
                 log_models[i].template_vector = log_template_embeddings[i]
 
     @staticmethod
-    async def filter_log_models_not_in_time_range(log_models: list[LogModel], time_start: datetime | None, time_end: datetime | None) -> list[LogModel]:
+    async def filter_log_models_not_in_time_range(
+        log_models: list[LogModel],
+        time_start: datetime | None,
+        time_end: datetime | None,
+    ) -> list[LogModel]:
         """获取日志模型的时间戳"""
         # 这里实现获取日志模型时间戳的具体逻辑
         log_models_in_time_range = []
@@ -97,7 +116,8 @@ class LogParser:
             end_time = None
             log_type: LogTypeEnum = log_model.log_type
             tiemstamp_regex = log_feature_class_mapping[log_type].capture_patterns.get(
-                LogValueEnum.TIMESTAMP.value, None)
+                LogValueEnum.TIMESTAMP.value, None
+            )
             if tiemstamp_regex is not None:
                 timestamp = re.search(tiemstamp_regex, log_model.content)
                 if timestamp is not None:
@@ -140,23 +160,36 @@ class LogParser:
                         "%m/%d/%Y %H:%M:%S.%f",
                         # 内核/系统日志简化格式（dmesg/ftrace）
                         "%s.%f",  # Unix时间戳+小数（如1710000000.123456）
-                        "%s"      # 纯Unix时间戳（10位数字）
+                        "%s",  # 纯Unix时间戳（10位数字）
                     ]
                     for time_fromat in time_format_list:
                         try:
-                            timestamp_dt = datetime.strptime(
-                                timestamp_str, time_fromat)
-                            start_time = min(
-                                start_time, timestamp_dt) if start_time is not None else timestamp_dt
-                            end_time = max(
-                                end_time, timestamp_dt) if end_time is not None else timestamp_dt
+                            timestamp_dt = datetime.strptime(timestamp_str, time_fromat)
+                            start_time = (
+                                min(start_time, timestamp_dt)
+                                if start_time is not None
+                                else timestamp_dt
+                            )
+                            end_time = (
+                                max(end_time, timestamp_dt)
+                                if end_time is not None
+                                else timestamp_dt
+                            )
                             break
                         except ValueError:
                             continue
             delta = timedelta(minutes=5)
-            if time_start is not None and start_time is not None and start_time > time_end + delta:
+            if (
+                time_start is not None
+                and start_time is not None
+                and start_time > time_end + delta
+            ):
                 continue
-            if time_end is not None and end_time is not None and end_time < time_start - delta:
+            if (
+                time_end is not None
+                and end_time is not None
+                and end_time < time_start - delta
+            ):
                 continue
             log_model.start_time = start_time
             log_model.end_time = end_time
@@ -165,11 +198,13 @@ class LogParser:
         return log_models_in_time_range
 
     @staticmethod
-    async def parse_log_file(file_path: str,
-                             need_split_by_regex: bool = False,
-                             time_start: datetime | None = None,
-                             time_end: datetime | None = None,
-                             chunk_size: int = 1024) -> list[LogModel]:
+    async def parse_log_file(
+        file_path: str,
+        need_split_by_regex: bool = False,
+        time_start: datetime | None = None,
+        time_end: datetime | None = None,
+        chunk_size: int = 1024,
+    ) -> list[LogModel]:
         log_lines = LogParser.read_log_file(file_path)
         log_models = []
         current = ""
@@ -187,11 +222,13 @@ class LogParser:
                         match_header_regex = header_regex
                         break
                 if log_type != LogTypeEnum.UNKNOWN and match_header_regex is not None:
-                    current = log_line+"\n"
+                    current = log_line + "\n"
                     index += 1
                     while index < len(log_lines):
                         is_continuous = False
-                        for continuous_regex in log_feature_class_mapping[log_type].mandatory[match_header_regex]:
+                        for continuous_regex in log_feature_class_mapping[
+                            log_type
+                        ].mandatory[match_header_regex]:
                             if re.search(continuous_regex, log_lines[index]):
                                 is_continuous = True
                                 break
@@ -200,34 +237,52 @@ class LogParser:
                             index += 1
                         else:
                             break
-                    log_models.append(LogModel(
-                        file_path=file_path,
-                        log_type=log_type,
-                        offset=offset,
-                        content=current.strip()))
+                    log_models.append(
+                        LogModel(
+                            file_path=file_path,
+                            log_type=log_type,
+                            offset=offset,
+                            content=current.strip(),
+                        )
+                    )
                     offset += 1
                 else:
-                    log_models.append(LogModel(
-                        file_path=file_path,
-                        log_type=log_type,
-                        offset=offset,
-                        content=log_line
-                    ))
+                    log_models.append(
+                        LogModel(
+                            file_path=file_path,
+                            log_type=log_type,
+                            offset=offset,
+                            content=log_line,
+                        )
+                    )
                     index += 1
                     offset += 1
         else:
             for log_line in log_lines:
                 if len(current) + len(log_line) > chunk_size:
-                    log_models.append(LogModel(
-                        file_path=file_path,
-                        log_type=LogParser.get_log_line_type(current),
-                        offset=offset,
-                        content=current
-                    ))
+                    log_models.append(
+                        LogModel(
+                            file_path=file_path,
+                            log_type=LogParser.get_log_line_type(current),
+                            offset=offset,
+                            content=current,
+                        )
+                    )
                     offset += 1
                     current = ""
                 current += log_line + "\n"
+            if current.strip() != "":
+                log_models.append(
+                    LogModel(
+                        file_path=file_path,
+                        log_type=LogParser.get_log_line_type(current),
+                        offset=offset,
+                        content=current,
+                    )
+                )
+                offset += 1
         if time_start is not None or time_end is not None:
             log_models = await LogParser.filter_log_models_not_in_time_range(
-                log_models, time_start, time_end)
+                log_models, time_start, time_end
+            )
         return log_models
